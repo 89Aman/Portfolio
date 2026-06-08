34011d3509bc6bb09880a2237fb3bae511010805
Aman Sharma <shasarita23@gmail.com>
2026-06-08 22:02:10 +0530

    Update hero.ts

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -10,7 +10,7 @@ import { LucideAngularModule, ArrowDown, Download, Github, Linkedin, Mail, Termi
   styleUrl: './hero.css',
 })
 export class HeroComponent implements OnInit, OnDestroy {
-  readonly RESUME_URL = 'https://drive.google.com/file/d/1LO0fVyxn3xZ_yaNzM4fX9Y1Dhz4KtrAY/view';
+  readonly RESUME_URL = 'https://drive.usercontent.google.com/u/0/uc?id=1LO0fVyxn3xZ_yaNzM4fX9Y1Dhz4KtrAY&export=download';
   readonly roles = [
     'RAG Systems Builder',
     'Backend Engineer',
